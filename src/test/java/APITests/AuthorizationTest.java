5c1b75a4337515c4764543ffae92078891fa7ba3
DeepikaJpr <deepika.chauhan.jpr@gmail.com>
2025-07-02 21:25:44 +0530

    pojo classes implementation - Deserialization and Added Jackson-databind maven dependency

--- src/test/java/APITests/AuthorizationTest.java
+++ src/test/java/APITests/AuthorizationTest.java
@@ -1,12 +1,20 @@
 package APITests;
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
+import org.testng.Assert;
+
 import io.restassured.path.json.JsonPath;
+import pojo.Api;
+import pojo.GetCourses;
+import pojo.WebAutomation;
 
 public class AuthorizationTest {
 
@@ -17,9 +25,11 @@ public class AuthorizationTest {
 		
 		String clientSecret = prop.getProperty("client_secret");
 		
-
+		String[] webAutomationcoursesTitleExpected= {"Selenium Webdriver Java","Cypress","Protractor"};
+		
+		//Post OAuth params and get access_token in response to access the Locked API
 		String response=
-			given()
+			given().log().all()
 				.param("client_id", "692183103107-p0m7ent2hk7suguv4vq22hjcfhcr43pj.apps.googleusercontent.com")
 				.param("client_secret",clientSecret )
 				.param("grant_type", "client_credentials")
@@ -32,13 +42,46 @@ public class AuthorizationTest {
 		JsonPath jspath = new JsonPath(response);
 		String token= jspath.getString("access_token");
 		
-		String response1=
-		given()
+		
+		//Get getCourseDetails using OAuth Token - access_token
+		GetCourses response1=
+		given().log().all()
 			.queryParam("access_token", token)
 		.when()
-			.get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").asString();
+			.get("https://rahulshettyacademy.com/oauthapi/getCourseDetails").as(GetCourses.class);
 		
-		System.out.println("-------------------"+response1+"----------------------");
+		
+		
+		//Print Course - `SoapUI Webservices testing` price
+		List<Api> apiCourses= response1.getCourses().getApi();
+		
+		for(int i=0;i<apiCourses.size();i++)
+		{
+			if(apiCourses.get(i).getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing"))
+			{
+				System.out.println("--------"+apiCourses.get(i).getPrice()+"--------------------");
+			}
+		}
+		
+		
+		
+		//Print all Web Automation Courses name
+		List<WebAutomation> webAutomationsCourses = response1.getCourses().getWebAutomation();
+		
+		ArrayList<String> wATA=new ArrayList<String>();
+		
+		for(int j=0;j<webAutomationsCourses.size();j++)
+		{
+			wATA.add(webAutomationsCourses.get(j).getCourseTitle());
+			System.out.println(webAutomationsCourses.get(j).getCourseTitle());
+			
+		}
+		
+		List<String> wATE= Arrays.asList(webAutomationcoursesTitleExpected);
+		Assert.assertTrue(wATA.equals(wATE));
+		
+		
+		
 	}
 
 }
